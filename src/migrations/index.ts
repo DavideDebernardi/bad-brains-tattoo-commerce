6efb9fb28ed0ca9821c855621ea4f811148a2d8d
davide <davide.debernardi@bevy-express.com>
2025-10-24 17:26:12 +0200

    feat: add migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251024_152541 from './20251024_152541';
+
+export const migrations = [
+  {
+    up: migration_20251024_152541.up,
+    down: migration_20251024_152541.down,
+    name: '20251024_152541'
+  },
+];
